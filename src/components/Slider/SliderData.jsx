d6636e5768b39d8907ca8976021e4e380bb1037d
AngelRuidiaz <ruidiazangel@gmail.com>
2023-02-05 18:54:56 -0300

    Se agrega componente SLider

---
+++ src/components/Slider/SliderData.jsx
@@ -0,0 +1,14 @@
+export const SliderData = [
+    {
+      image:
+        "https://images.nvidia.com/aem-dam/Solutions/geforce/ada/rtx-4090/geforce-rtx-4090-product-gallery-full-screen-3840-3.jpg",
+    },
+    {
+      image:
+        "https://teknogamers.com/wp-content/uploads/2019/05/AMD-X499-HEDT-Platform-Ryzen-Threadripper-CPUs.png",
+    },
+    {
+      image:
+        "https://cdn.mos.cms.futurecdn.net/dNvmj6TZK73DkbCKhw6pA5.png",
+    },
+]
