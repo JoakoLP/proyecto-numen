eb89cab3bf58e2f06b8c19b0b283d7140891869e
nahir24 <nahirsoto24@gmail.com>
2023-02-07 22:26:25 -0300

    navbar-slide-alternativa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Header from "./components/Header";
 import Cards from "./components/Cards";
+import navbar from "./components/navbar";
+import carousel from "./components/carousel";
 
 function App() {
   return (
     <div>
       <Header />
       <Cards />
+      <navbar/>
+      <carousel/>
+
     </div>
   );
 }
