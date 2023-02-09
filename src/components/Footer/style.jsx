f931e31c386eb15fee5967c3d4822350b851a4b8
Florencia <fd.lopez@outlook.com>
2023-02-08 21:00:50 -0300

    Footer actualizado

---
+++ src/components/Footer/style.jsx
@@ -0,0 +1,3 @@
+export const Button = "h-16";
+export const TitleFoot = "text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-900 to-green-600";
+export const UlFoot = "text-xl"
