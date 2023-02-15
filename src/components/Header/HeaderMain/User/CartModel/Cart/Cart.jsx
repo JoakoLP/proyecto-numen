971875b92b0fa35f63f9b0adb4de0bdb282ca79b
JoakoLP <joakotakara@gmail.com>
2023-02-15 19:48:21 -0300

    import fix

--- src/components/Header/HeaderMain/User/CartModel/Cart/Cart.jsx
+++ src/components/Header/HeaderMain/User/CartModel/Cart/Cart.jsx
@@ -1,10 +1,10 @@
 import React, { useReducer } from "react";
 import { TYPES } from "./actions";
-import { initialState, cartReducer } from "./CartReducer";
+import { initialState, cartReducer } from "./cartReducer";
 // import * as styles from "./styles";
 
 const Cart = () => {
-  const [state, dispatch] = useReducer(CartReducer, initialState);
+  const [state, dispatch] = useReducer(cartReducer, initialState);
 
   return (
     <div>
