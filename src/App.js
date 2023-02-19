2f605874de59f612716cd2bf80a17227fca052ff
AngelRuidiaz <ruidiazangel@gmail.com>
2023-02-19 18:55:52 -0300

    Se elimina  componente categoria y se lo reemplaza por componente producto

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import Header from "./components/Header";
 import Cards from "./components/Cards";
 import Footer from "./components/Footer";
 import Slider from "./components/Slider/Slider";
-import Categories from "./components/Categories/Categories";
+import Productos from "./components/Productos/productos";
 
 function App() {
   return (
     <div>
       <Header />
-      <Cards />
       <Slider />
-      <Categories />
+      <Productos />
       <Footer />
     </div>
   );
