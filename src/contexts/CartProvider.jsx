0c08bc7986c5366273c89a3b348d790d4eaa4c5d
JoakoLP <joakotakara@gmail.com>
2023-02-23 14:12:21 -0300

    axios and console log removed

--- src/contexts/CartProvider.jsx
+++ src/contexts/CartProvider.jsx
@@ -2,36 +2,11 @@ import { createContext, useEffect, useReducer } from "react";
 import { initialState } from "../components/Cart/cartReducer";
 import cartReducer from "../components/Cart/cartReducer";
 import { TYPES } from "../components/Cart/actions";
-import axios from "axios";
 
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(cartReducer, initialState);
-
-  // Por qué json-server tira error 404 manifest.json??
-  // const updateState = async () => {
-  //   const productsURL = "https://localhost:300/products";
-  //   const cartURL = "https://localhost:300/cart";
-  //   const totalURL = "https://localhost:300/total";
-  //   const countURL = "https://localhost:300/count";
-  //   const resProducts = await axios.get(productsURL);
-  //   const resCart = await axios.get(cartURL);
-  //   const resTotal = await axios.get(totalURL);
-  //   const resCount = await axios.get(countURL);
-  //   const newProduct = await resProducts.data;
-  //   const newCartItem = await resCart.data;
-  //   const newTotal = await resTotal.data;
-  //   const newCount = await resCount.data;
-
-  //   dispatch({
-  //     type: TYPES.READ_STATE,
-  //     payload: [newProduct, newCartItem, newTotal, newCount],
-  //   });
-  // };
-  // useEffect(() => {
-  //   updateState();
-  // }, []);
 
   const actions = {
     addToCart: (id) => {
