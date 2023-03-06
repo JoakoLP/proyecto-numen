4e0a419369b3455edb69bc99c576cace9bd0d9e9
Noah <noenavarrete71@outlook.com>
2023-03-05 21:43:00 -0300

    se modifica los pequeños divs visibles

--- src/components/Comentary/Comentary.jsx
+++ src/components/Comentary/Comentary.jsx
@@ -21,33 +21,32 @@ const Comentary = () => {
           </p>
         </button>
       </div>
-      <div className=" fixed z-10 inset-x-1 top-64 mt-56 flex flex-row-reverse rounded-lg ">
+      <div className="fixed z-10 inset-x-1 top-64 mt-56 flex flex-row-reverse rounded-lg ">
         <form
           className={`${
-            !open ? "w-60 md:w-80" : "w-0 border-0"
-          } max-h-96 bg-purple-300 bg-opacity-40 border-2 border-purple-900 rounded-lg flex flex-col transition-all duration-500`}
+            !open ? "w-60 md:w-80 border-2" : "w-0  "
+          } max-h-96 bg-purple-300 bg-opacity-40 border-purple-900 rounded-lg flex flex-col transition-all duration-500`}
           onSubmit={(ev) => {
             ev.preventDefault();
             setSearch([...search, ev.target.search.value]);
           }}
         >
-          <div className="flex justify-start pl-1 w-80">
+          <div className={`flex justify-start pl-1 w-80`}>
             <button
-              className={
-                "bg-red-400 p-1 mr-14 md:mr-20 z-90 bg-purple-600 text-white rounded-lg"
-              }
+              className={`
+               bg-red-400 p-1 mr-14 md:mr-20 z-90 bg-purple-600 text-white rounded-lg`}
               onClick={() => setOpen(true)}
             >
               x
             </button>
-            <p className=" text-white  text-lg font-medium">Comentarios</p>
+            <p className="text-white  text-lg font-medium">Comentarios</p>
           </div>
           <textarea
             rows="60"
             cols="40"
             className={`${
-              !open ? "w-57 md:w-76" : "w-0 hidden"
-            } max-h-20 mb-1 px-2 hover:bg-purple-200 rounded-lg border-2 bg-slate-100 border-purple-500 shadow-black shadow-sm `}
+              !open ? "w-57 border-2 px-2 mb-1 md:w-76" : ""
+            } max-h-20 hover:bg-purple-200 rounded-lg  bg-slate-100 border-purple-500 shadow-black shadow-sm `}
             type="text"
             name="search"
             required
@@ -59,13 +58,13 @@ const Comentary = () => {
           <button
             type="submit"
             className={`${
-              !open ? "w-50" : "w-0 hidden"
-            } m-1 text-white rounded-full whitespace-nowrap bg-[#8a4af3] hover:bg-purple-900 font-bold sm:text-xs py-2 mb-2 px-4 duration-300 hover:shadow-inner hover:shadow-neutral-800`}
+              !open ? "w-50" : " "
+            } m-1 text-white rounded-full whitespace-nowrap bg-[#8a4af3] hover:bg-purple-900 font-bold sm:text-xs py-2 mb-2 px-4 transition-all duration-500 hover:shadow-inner hover:shadow-neutral-800`}
           >
             Comentar
           </button>
           <div className=" overflow-x-hidden rounded-xl">
-            <ul className="mr-1 rounded-xl overflow-y-hidden overflow-x-hidden gitflex flex-col flex-wrap space-y-1  ">
+            <ul className=" mr-1 rounded-xl overflow-y-hidden overflow-x-hidden gitflex flex-col flex-wrap space-y-1  ">
               {search
                 .slice(0)
                 .reverse()
