eded2cde9a4d3c40469ea61f4a39a1d68255e871
JoakoLP <joakotakara@gmail.com>
2023-03-06 19:24:35 -0300

    cart modal onClick close

--- src/components/Header/HeaderMain/User/CartModel/ModalContainer.jsx
+++ src/components/Header/HeaderMain/User/CartModel/ModalContainer.jsx
@@ -19,16 +19,21 @@ const ModalContainer = ({ children, status, setStatus }) => {
       <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
     </svg>
   );
+  const handleChildElementClick = (e) => {
+    e.stopPropagation();
+  };
   return (
     <>
       {status && (
         <div
           className={styles.overlay}
           style={{ background: "rgba(0, 0, 0, 0.5)" }}
+          onClick={() => setStatus(!status)}
         >
           <div
             className={styles.modCont}
             style={{ boxShadow: "rgba(100,100,111, 0.2) 0px 7px 29px 0px" }}
+            onClick={(e) => handleChildElementClick(e)}
           >
             <div className={styles.modHeader}>
               <h3 className={styles.modTitle}>Carrito de Compras</h3>
