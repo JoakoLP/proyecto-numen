c815bc728fc5a0f8f3a21a8ec28c7e6207334ff1
JoakoLP <joakotakara@gmail.com>
2023-03-07 12:37:48 -0300

    app

--- src/App.js
+++ src/App.js
@@ -29,7 +29,6 @@ function App() {
                   </>
                 }
               />
-
               <Route path="/about-us" element={<About />} />
             </Routes>
           </div>
